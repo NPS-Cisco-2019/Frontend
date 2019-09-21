24f1d0ea938bfebd9a801bb1205dbdb0ba0c5e45
Lutetium-Vanadium <luv.s7000@gmail.com>
2019-09-21 21:51:19 +0530

    fixed issues with chrome on android

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 const selected = true;
-let cls = 'nav-obj flash';
+let cls = '';
 if (selected){
-  cls += ' selected'
+  cls += 'selected'
 };
+
+let maxLength = (10/100) * (50/100) * window.innerHeight;
+
+const navObj = {
+  'align-items': 'center',
+  'justify-items': 'center',
+  'padding': '10%',
+  'height': maxLength,
+  'border-radius': '50%',
+  'width': maxLength
+}
+// className="nav-obj"
 
 function App() {
   return (
     <div className="App">
       <div className="background">
         <header className="nav">
-            <div className={cls}><img src={require("./flash.png")} alt="flash" className="nav-img" /></div>
-            <div className="nav-obj settings"><img src={require("./settings.png")} alt="settings" className="nav-img" /></div>
-            <div className="nav-obj gallery"><img src={require("./gallery.png")} alt="Gallery" className="nav-img" /></div>
+            <div className={cls} style={navObj}><img src={require("./flash.png")} alt="flash" className="nav-img" /></div>
+            <div style={navObj}><img src={require("./settings.png")} alt="settings" className="nav-img" /></div>
+            <div style={navObj}><img src={require("./gallery.png")} alt="Gallery" className="nav-img" /></div>
         </header>
         <img src="https://i.redd.it/xcmltqxm69n31.jpg" alt="meme" className="center" />
         <footer>
