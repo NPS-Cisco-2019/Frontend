bc53e24dd4ca1485c17d1400518c7cf82b788b7a
Lutetium-Vanadium <luv.s7000@gmail.com>
2019-10-10 20:49:39 +0530

    small bug

--- src/appComponents/settings.js
+++ src/appComponents/settings.js
@@ -24,7 +24,7 @@ class SettingsPage extends React.Component {
                 top: Math.round(window.innerHeight/10),
                 marginLeft: window.innerWidth / 40
             },
-            bannerChildOpacity: 1
+            bannerChildAnimation: ''
         }
 
         this.tutClick = this.tutClick.bind(this);
@@ -47,11 +47,11 @@ class SettingsPage extends React.Component {
                 marginLeft: 0,
                 top: 0
             },
-            bannerChildOpacity: 0
+            bannerChildAnimation: 'lateFade'
         })
         setTimeout(() => {
             this.props.history.push('/Tutorial')
-        }, 600)
+        }, 800)
     }
 
     closeBanner(){
@@ -70,7 +70,7 @@ class SettingsPage extends React.Component {
                 <div style={{display: this.state.showTutBanner ? 'auto' : 'none',
                             ...this.state.bannerStyle}}
                     className={`link-container-wrapper ${this.state.animateOutBanner ? 'fadeout' : null}`}>
-                    <div className="link-container" style={{opacity: this.state.bannerChildOpacity}}>
+                    <div className={`link-container ${this.state.bannerChildAnimation}`}>
                         <div className="link-to-tut" onClick={this.tutClick}>
                             Want to install this app?<br/>Click here to find out how.
                         </div>
@@ -84,7 +84,7 @@ class SettingsPage extends React.Component {
                             position: "relative",
                             transition: 'all 300ms cubic-bezier(0.215, 0.610, 0.355, 1)'}}>
                     <img src={require('./pictures/default.jpg')} alt="meme" style={{maxWidth: window.innerWidth, postion: "absolute"}} />
-                    <p style={{marginTop: 30}}>Temporary until a settings page and link to tutorial page is made</p>
+                    <p style={{marginTop: 30}}>Temporary until a settings are implemented</p>
                 </div>
             </div>
         )
