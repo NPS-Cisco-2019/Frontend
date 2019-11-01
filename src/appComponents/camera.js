84b49541750e3c6d708b136a51e401aff17cff70
Lutetium-Vanadium <luv.s7000@gmail.com>
2019-11-01 13:15:32 +0530

    Added flash

--- src/appComponents/camera.js
+++ src/appComponents/camera.js
@@ -16,11 +16,20 @@ export default function Camera({ error }) {
             navigator.mediaDevices.getUserMedia(constraints)
                 .then(stream => {
 
-                    let video = document.getElementById('camera');
+                    let video = document.getElementById('Flash');
+
+                    track = stream.getVideoTracks()[0];
 
 
                     video.srcObject = stream;
                     // video.src = track;
+
+                    btn = document.getElementById('camera');
+                    btn.addEventListener('click', () => {
+                        track.applyConstraints({
+                            advanced: [{ torch: true }]
+                        })
+                    })
                 })
                 .catch(err => {
                     error(err);
